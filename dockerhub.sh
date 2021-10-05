61f8a6b7b869f1255763e4cb519f51f22295a802
James Lockhart <james.lockhart@sykescottages.co.uk>
2021-10-05 19:44:02 +0100

    Enabled local builds of dockerhub

---
+++ dockerhub.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Validate the container passes our tests
+#./test.sh
+
+# You need to provide your own creds because #security
+docker login
+
+VERSIONS=( "5.6" "7.0" "7.1" "7.2" "7.3" "7.4" "8.0" )
+for VERSION in "${VERSIONS[@]}"
+do
+  docker build --no-cache -t sykescottages/php:${VERSION}-fpm $VERSION/fpm
+  docker push sykescottages/base:${VERSION}-fpm
+
+  docker build --no-cache -t sykescottages/php:${VERSION}-cli $VERSION/cli
+  docker push sykescottages/base:${VERSION}-cli
+done
--- test.sh
+++ test.sh
@@ -1,22 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 docker-compose -f base/docker-compose.test.yml build --no-cache && \
-    docker-compose -f base/docker-compose.test.yml up
+    docker-compose -f base/docker-compose.test.yml run --rm sut
 
-docker-compose -f 5.6/cli/docker-compose.test.yml build --no-cache && \
-    docker-compose -f 5.6/cli/docker-compose.test.yml up
-
-docker-compose -f 7.0/cli/docker-compose.test.yml build --no-cache && \
-    docker-compose -f 7.0/cli/docker-compose.test.yml up
-
-docker-compose -f 7.1/cli/docker-compose.test.yml build --no-cache && \
-    docker-compose -f 7.1/cli/docker-compose.test.yml up
-
-docker-compose -f 7.2/cli/docker-compose.test.yml build --no-cache && \
-    docker-compose -f 7.2/cli/docker-compose.test.yml up
-
-docker-compose -f 7.3/cli/docker-compose.test.yml build --no-cache && \
-    docker-compose -f 7.3/cli/docker-compose.test.yml up
-
-docker-compose -f 7.4/cli/docker-compose.test.yml build --no-cache && \
-    docker-compose -f 7.4/cli/docker-compose.test.yml up
+VERSIONS=( "5.6" "7.0" "7.1" "7.2" "7.3" "7.4" "8.0" )
+for VERSION in "${VERSIONS[@]}"
+do
+  docker-compose -f $VERSION/cli/docker-compose.test.yml build --no-cache && \
+    docker-compose -f $VERSION/cli/docker-compose.test.yml run --rm sut
+done
