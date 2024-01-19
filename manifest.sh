08c6487872695df6d729dd70e18a2f33df258e79
James Lockhart <james.lockhart@sykescottages.co.uk>
2024-01-19 13:20:30 +0000

    Remove manifest before creating new one

--- manifest.sh
+++ manifest.sh
@@ -8,6 +8,7 @@ BASE_VERSIONS=($(find . -type d -regex '\.*/[0-9]+-base' | sort | sed 's/\.\///'
 for VERSION in "${BASE_VERSIONS[@]}"
 do
   TAG="sykescottages/php:$VERSION"
+  docker manifest rm $TAG
   docker manifest create $TAG \
     --amend "$TAG-amd64" \
     --amend "$TAG-arm64"
@@ -18,12 +19,14 @@ VERSIONS=($(find . -type d -regex '\.*/[0-9]+\.[0-9]+' | sort | sed 's/\.\///'))
 for VERSION in "${VERSIONS[@]}"
 do
   TAG="sykescottages/php:${VERSION}-cli"
+  docker manifest rm $TAG
   docker manifest create $TAG \
     --amend "$TAG-amd64" \
     --amend "$TAG-arm64"
   docker manifest push $TAG
 
   TAG="sykescottages/php:${VERSION}-fpm"
+  docker manifest rm $TAG
   docker manifest create $TAG \
     --amend "$TAG-amd64" \
     --amend "$TAG-arm64"
