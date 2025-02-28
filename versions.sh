deaebf7794d8c9f4937f11074ff65cf01d8572cf
James Lockhart <james.lockhart@sykescottages.co.uk>
2025-02-28 16:23:36 +0000

    Package updates

--- versions.sh
+++ versions.sh
@@ -4,11 +4,11 @@ VERSIONS=($(find . -type d -regex '\.*/[0-9]+\.[0-9]+' | sort | sed 's/\.\///'))
 
 for VERSION in "${VERSIONS[@]}"
 do
-  docker run -it sykescottages/php:${VERSION}-fpm bash -c "apt -qq list | tail -n+2" | tail -n+4 > $VERSION/fpm/aptitude-itinerary
-  docker run -it sykescottages/php:${VERSION}-cli bash -c "apt -qq list | tail -n+2" | tail -n+4 > $VERSION/cli/aptitude-itinerary
+  docker run --pull always -it sykescottages/php:${VERSION}-fpm bash -c "apt -qq list | tail -n+2" | tail -n+4 > $VERSION/fpm/aptitude-itinerary
+  docker run --pull always -it sykescottages/php:${VERSION}-cli bash -c "apt -qq list | tail -n+2" | tail -n+4 > $VERSION/cli/aptitude-itinerary
 done
 
 for VERSION in "${VERSIONS[@]}"
 do
-  docker run sykescottages/php:${VERSION}-cli -v|grep -E "^PHP "
+  docker run --pull always sykescottages/php:${VERSION}-cli -v|grep -E "^PHP "
 done
